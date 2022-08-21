c35e7c698abd0fe2ae48811b13cb4c16594966a9
Hugo Sampaio <hugo@deshtec.com.br>
2022-08-21 16:02:06 -0300

    tx as obj, first m-n exaust search

--- simulations/qpsk-tx-fixedpoint.py
+++ simulations/qpsk-tx-fixedpoint.py
@@ -10,41 +10,58 @@ class qpsk_tx:
     fsamples = 1 # assume our sample rate is 1 Hz
     Tsample = 1/fsamples # calc sample period
     Tsymbol = Tsample*sps
-
-    def __init__(self, bins = 50, srrc_taps = 43, beta = 0.50):
+    x = np.array([])
+    hsrrc = None
+    hsrrc_fp = []
+    x_fp = []
+    tx_signal = None
+    tx_signal_fp = None
+    def __init__(self, m, n, bins = 50, srrc_taps = 43, beta = 0.50):
         num_symbols = int(bins*self.fsamples)
-
         in_bits = np.random.randint(0, 2, num_symbols) # Our data to be transmitted, 1's and 0's
-
-        x = np.array([])
         for bit in in_bits:
-            pulse = np.zeros(sps)
+            pulse = np.zeros(self.sps)
             pulse[0] = bit*2-1 # set the first value to either a 1 or -1
-            x = np.concatenate((x, pulse)) # add the 8 samples to the signal
+            self.x = np.concatenate((self.x, pulse)) # add the 8 samples to the signal
 
-        _, hsrrc = filter.rrcosfilter(srrc_taps, beta, self.Tsymbol, self.fsamples)
+        _, self.hsrrc = filter.rrcosfilter(srrc_taps, beta, self.Tsymbol, self.fsamples)
 
-        hsrrc_fp = []
-        for a in hsrrc:
-            hsrrc_fp.append(fp(a, signed=1, m=2, n=8, rounding='convergent'))
+        for a in self.hsrrc:
+            self.hsrrc_fp.append(fp(a, signed=1, m=m, n=n, rounding='convergent'))
         
-        x_fp = []
-        for a in x:
-            x_fp.append(fp(a, signed=1, m=2, n=0, rounding='convergent'))
+        for a in self.x:
+            self.x_fp.append(fp(a, signed=1, m=m, n=n, rounding='convergent'))
 
     def convolve(self):
-        tx_signal = np.convolve(self.x, self.hsrrc)
-        tx_signal_fp = np.convolve(self.x_fp, self.hsrrc_fp)
+        self.tx_signal = np.convolve(self.x, self.hsrrc)
+        self.tx_signal_fp = np.convolve(self.x_fp, self.hsrrc_fp)
 
     def sqnr(self): 
-        quant_err = tx_signal-tx_signal_fp
+        quant_err = self.tx_signal - self.tx_signal_fp
         mean = statistics.mean([float(fp) for fp in quant_err])
         stdev = statistics.stdev([float(fp) for fp in quant_err])
         #print(mean, stdev)
         #print (f'{hsrrc_fp[0]:q}, {x_fp[0]:q}, {tx_signal_fp[0]:q}')
 
         p_noise = float(np.sum(quant_err**2)) / len(quant_err)
-        p_x = np.sum(x**2) / len(x)
+        p_x = np.sum(self.x**2) / len(self.x)
         sqnr = 10*np.log10(p_x/p_noise)
         return sqnr
 
+LIMIT_SQNR = 60.0
+step_sqnr = 0.0
+m = 2
+n = 32
+sim = qpsk_tx(m,n)
+while(1):
+    sim.convolve()
+    sim_sqnr = sim.sqnr()
+    if (sim_sqnr < LIMIT_SQNR):
+        print(sim_sqnr, m, n)
+        break
+    step_sqnr = sim_sqnr
+    n -= 1
+    for i in range(len(sim.hsrrc_fp)):
+        sim.hsrrc_fp[i].resize(m, n)
+
+print(step_sqnr, m, n+1)
