e3392a7351f632f591c80e2ff5e3eb2f2cab21bf
Hugo Sampaio <hugo@deshtec.com.br>
2022-08-27 16:10:15 -0300

    added step_convolve

--- simulations/qpsk-tx-fixedpoint.py
+++ simulations/qpsk-tx-fixedpoint.py
@@ -5,6 +5,26 @@ import commpy.filters as filter
 from fixedpoint import FixedPoint as fp
 import statistics
 
+
+def step_convolve(signal, filter):
+    sum = [0]*(len(signal)+len(filter)-1)
+    step_sum = fp(0, signed=1, m=m, n=n, rounding='convergent')
+    _signal = list(signal)
+    _signal.extend( [fp(0, signed=1, m=m, n=n, rounding='convergent')]*len(filter))
+    _filter = list(filter)
+    _filter.extend([fp(0, signed=1, m=m, n=n, rounding='convergent')]*len(signal))
+    for index_signal in range(0,len(signal)+len(filter)-1):
+        for index_filter in range(0,index_signal+1):
+            step_sum += _signal[index_signal-index_filter] * _filter[index_filter]
+        sum[index_signal]=step_sum
+        #print(float(step_sum))
+        step_sum = fp(0, signed=1, m=m, n=n, rounding='convergent')
+    return sum
+        
+
+
+
+
 class qpsk_tx:
     sps = 8
     fsamples = 1 # assume our sample rate is 1 Hz
@@ -16,6 +36,7 @@ class qpsk_tx:
     x_fp = []
     tx_signal = None
     tx_signal_fp = None
+    tx_signal_fp_convolve = None
     def __init__(self, m, n, bins = 50, srrc_taps = 43, beta = 0.50):
         num_symbols = int(bins*self.fsamples)
         in_bits = np.random.randint(0, 2, num_symbols) # Our data to be transmitted, 1's and 0's
@@ -38,21 +59,32 @@ class qpsk_tx:
 
     def sqnr(self): 
         quant_err = self.tx_signal - self.tx_signal_fp
-        mean = statistics.mean([float(fp) for fp in quant_err])
-        stdev = statistics.stdev([float(fp) for fp in quant_err])
+        #mean = statistics.mean([float(fp) for fp in quant_err])
+        #stdev = statistics.stdev([float(fp) for fp in quant_err])
         #print(mean, stdev)
         #print (f'{hsrrc_fp[0]:q}, {x_fp[0]:q}, {tx_signal_fp[0]:q}')
-
+        #print(quant_err)
+        #print(type(quant_err[0]))
         p_noise = float(np.sum(quant_err**2)) / len(quant_err)
         p_x = np.sum(self.x**2) / len(self.x)
         sqnr = 10*np.log10(p_x/p_noise)
+        #print(p_noise, p_x)
         return sqnr
 
-LIMIT_SQNR = 60.0
+LIMIT_SQNR = 40.0
 step_sqnr = 0.0
 m = 2
 n = 32
 sim = qpsk_tx(m,n)
+#sim.convolve()
+#sim.sqnr()
+#plt.figure(1)
+#plt.plot(sim.tx_signal, '.-')
+#plt.plot(sim.tx_signal_fp, '.')
+#plt.figure(2)
+#plt.plot(sim.tx_signal_fp, '.-')
+#plt.plot(sim.tx_signal_fp_convolve, '.')
+#plt.show()
 while(1):
     sim.convolve()
     sim_sqnr = sim.sqnr()
@@ -63,5 +95,4 @@ while(1):
     n -= 1
     for i in range(len(sim.hsrrc_fp)):
         sim.hsrrc_fp[i].resize(m, n)
-
 print(step_sqnr, m, n+1)
